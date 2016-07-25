d7e38fdd79475b731be99117a3d97b389e3589d4
robertweber95 <robertweber95@gmail.com>
2016-07-24 22:04:33 -0700

    Added gulp default task

--- gulpfile.js
+++ gulpfile.js
@@ -27,8 +27,12 @@ gulp.task('open', ['connect'], function() {
   gulp.src('dist/index.html')
       .pipe(open({uri: config.devBaseUrl + ':' + config.port + '/'}));
 });
+
 gulp.task('html', function() {
   gulp.src(config.paths.html)
       .pipe(gulp.dest(config.paths.dist))
       .pipe(connect.reload());
-})
+});
+
+// Default Gulp task for compiling and deploying to server
+gulp.task('default', ['html', 'open']);
