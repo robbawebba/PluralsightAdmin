7af8f28e9d3d9cb201182d752d6f53d4a8b92699
robertweber95 <robertweber95@gmail.com>
2016-07-24 21:41:25 -0700

    Created gulpfile.js with task for connecting to dev server

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect'); // Runs a local dev server
+var open = require('gulp-open'); // Open a URL in a web browser
+
+var config = {
+  port: 9005,
+  devBaseUrl: 'http://localhost',
+  paths: {
+    html: './src/*.html'
+    dist: './dist'
+  }
+}
+
+// Start a local development server
+gulp.task('connect', function() {
+  connect.server({
+    root: ['dist'],
+    port: config.port,
+    base: config.devBaseUrl,
+    livereload: true
+  });
+});
