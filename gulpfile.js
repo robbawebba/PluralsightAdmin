6350f59b16c59b4fbf92f36693535d5a42773f25
robertweber95 <robertweber95@gmail.com>
2016-07-24 22:02:09 -0700

    Created gulp task html

--- gulpfile.js
+++ gulpfile.js
@@ -25,5 +25,10 @@ gulp.task('connect', function() {
 
 gulp.task('open', ['connect'], function() {
   gulp.src('dist/index.html')
-      .pipe(open({uri: config.devBaseUrl + ':' + config.port + '/'}))
+      .pipe(open({uri: config.devBaseUrl + ':' + config.port + '/'}));
+});
+gulp.task('html', function() {
+  gulp.src(config.paths.html)
+      .pipe(gulp.dest(config.paths.dist))
+      .pipe(connect.reload());
 })
